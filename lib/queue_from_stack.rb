0b2b5e9ccaf56c1ad1be194be22d74d40085cca4
ian gottheim <iangottheim@gmail.com>
2022-10-23 12:36:10 -0400

    commit tree 4d8b67a686b81de1001345eebbdf5c4c0a2d94ab

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,33 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+    
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+      end
+
+
+      def add(value)
+        # If we start with the following values before calling `#add(3)`
+        # @s1: [2, 1]    @s2: []
+        # First, take all the items out of @s1 and move them to @s2: 
+        # @s1: []        @s2: [1, 2]
+        @s2.push(@s1.pop) until @s1.empty? 
+        # Next, add a new item to the now-empty @s1: 
+        # @s1: [3]       @s2: [1, 2]
+        @s1.push(value)
+        # Finally, return all items to @s1 in the order they were removed: 
+        # @s1: [3, 2, 1] @s2: []
+        @s1.push(@s2.pop) until @s2.empty? 
+      end
+    
+      def remove
+        @s1.pop
+      end
+       
+      def peek
+        @s1.peek
+      end
+
+end
